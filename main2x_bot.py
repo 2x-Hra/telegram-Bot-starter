984d8f8c6359a1ec6a6344cad942e207eef9f482
hrakbari77@gmail.com <hrakbari77@gmail.com>
2021-02-17 19:09:49 +0330

    bot token added

--- main2x_bot.py
+++ main2x_bot.py
@@ -2,6 +2,7 @@ from pyrogram import Client
 
 app = Client(
     "my_bot",
+    bot_token='1597619460:AAHSfzUFrAcjlqe7SpKK-PEUzN5lfo9bon4',
     api_id= 3477634,
     api_hash= "b892572907ad5fbce8a89a3ea947ed62"
 )
