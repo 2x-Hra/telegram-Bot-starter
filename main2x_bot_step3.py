ba717f749edfe7dd46422bdea865b98a8fe35efd
hrakbari77@gmail.com <hrakbari77@gmail.com>
2021-02-18 22:38:46 +0330

    step3 worked

--- main2x_bot_step3.py
+++ main2x_bot_step3.py
@@ -8,13 +8,13 @@ data =[]
 
 def IKM(data):
     '''
-        Explanation :
-            
-
+        Explanation : Create a inLine button with name :text and key : cbd
+        
     '''
     return InlineKeyboardMarkup([[InlineKeyboardButton(text, cbd)] for text, cbd in data])
 
 MAIN_KEYBOARD = ReplyKeyboardMarkup([['setname','setage'],['my profile']],resize_keyboard=True)
+TEACHERS_INLINEKB = IKM( [('Dr Keshtkaran', 'TCH0'), ('Dr Hamze', 'TCH1'), ('Dr Sami', 'TCH2')] )
 class MyUser:
     def __init__(self, user_id):
         self.id = user_id
@@ -28,11 +28,10 @@ def check_user(user_id): # check if user exists ,if not user will added to data
             return user
     new_user = MyUser(user_id)
     data.append(new_user)
+    return new_user
 
 @app.on_message()
 def handle_message(bot: Client , msg: Message):
-
-
     user = check_user(msg.from_user.id)
     if (msg.chat.type != 'private'): #
         return
@@ -40,20 +39,22 @@ def handle_message(bot: Client , msg: Message):
     if msg.text:
         if (msg.text == '/start' ):
             user.state = 0
-            bot.send_message(user.id, 'welcome' , reply_markup=MAIN_KEYBOARD )
+            bot.send_message(user.id, 'welcome' , reply_markup=TEACHERS_INLINEKB )
 
  
 
-teachers = ["dr hamze","dr kesht", "dr sami" ] #database simulator
+
+teachers = ['دکتر کشتکاران', 'دکتر حمزه', 'دکتر سامی'] #database simulator
+ 
 
 @app.on_callback_query()
 def handle_callback(bot: Client, query: CallbackQuery):
-    bot.answer_callback_query(query.id, f"got {query.data}!!", show_alert=True) # show alert is for POP message
-    
+    if query.data.startswith('TCH'):
+        i = int(query.data[3:])
+        bot.edit_message_text(query.message.chat.id, query.message.message_id, teachers[i],
+                              reply_markup=TEACHERS_INLINEKB)
 
 
-
-
-    
+ 
 
 app.run()
