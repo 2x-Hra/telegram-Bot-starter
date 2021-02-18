3015cd1588bc46a25884b714c3cb9d05ae9c9d69
hrakbari77@gmail.com <hrakbari77@gmail.com>
2021-02-18 15:34:59 +0330

    2if added

--- main2x_bot.py
+++ main2x_bot.py
@@ -2,7 +2,7 @@ from pyrogram import Client
 from  pyrogram.types import Message
 
 app = Client(
-    "my_bot",
+    session_name="my_bot",
     bot_token='1597619460:AAHSfzUFrAcjlqe7SpKK-PEUzN5lfo9bon4',
     api_id= 3477634,
     api_hash= "b892572907ad5fbce8a89a3ea947ed62"
@@ -10,10 +10,16 @@ app = Client(
 
 @app.on_message()
 
-def send_message(client: Client , msg: Message):
+def send_message(bot: Client , msg: Message):
     user_id = msg.from_user.id
     bot_text = msg.text
-    client.send_message(user_id, bot_text)
+    
+    if msg.text:
+        bot.send_message(user_id, bot_text)
+    elif msg.voice:
+        print(msg.voice)
+
+
     
 
 app.run()
