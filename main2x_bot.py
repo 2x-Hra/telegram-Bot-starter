6fe77503d3e6df4c4c98b29b2c42d4b818710bd1
hrakbari77@gmail.com <hrakbari77@gmail.com>
2021-02-18 16:30:00 +0330

    photo added

--- main2x_bot.py
+++ main2x_bot.py
@@ -1,12 +1,7 @@
 from pyrogram import Client
 from  pyrogram.types import Message
 
-app = Client(
-    session_name="my_bot",
-    bot_token='1597619460:AAHSfzUFrAcjlqe7SpKK-PEUzN5lfo9bon4',
-    api_id= 3477634,
-    api_hash= "b892572907ad5fbce8a89a3ea947ed62"
-)
+app = Client("my_bot")
 
 @app.on_message()
 
@@ -17,7 +12,12 @@ def send_message(bot: Client , msg: Message):
     if msg.text:
         bot.send_message(user_id, bot_text)
     elif msg.voice:
-        print(msg.voice)
+
+        print(msg.voice.file_id)
+        bot.send_voice(user_id, msg.voice.file_id)
+    elif msg.photo:
+        bot.send_message(user_id, msg.photo.file_id)
+
 
 
     
