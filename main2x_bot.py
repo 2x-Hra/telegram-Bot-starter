424584dc54a9afc1cebf8651d1cb95810744cb36
hrakbari77@gmail.com <hrakbari77@gmail.com>
2021-02-18 17:19:21 +0330

    keyboard fixed

--- main2x_bot.py
+++ main2x_bot.py
@@ -5,12 +5,13 @@ from  pyrogram.types import Message, ReplyKeyboardMarkup, KeyboardButton ,ReplyK
 app = Client("my_bot")
 
 data =[]
+MAIN_KEYBOARD = ReplyKeyboardMarkup([['setname','setage'],['my profile']],resize_keyboard=True)
 class MyUser:
     def __init__(self, user_id):
         self.id = user_id
         self.state = 0
-        self.name
-        self.age 
+        self.name = None
+        self.age = None
 
 def check_user(user_id): # check if user exists ,if not user will added to data
     for user in data:
@@ -30,36 +31,34 @@ def handle_message(bot: Client , msg: Message):
     if msg.text:
         if (msg.text == '/start' ):
             bot.send_message(user.id, 'welcome' ,
-                                reply_markup=ReplyKeyboardMarkup([['setname','setage'],['my profile']],resize_keyboard=True)
+                                reply_markup=MAIN_KEYBOARD
                             )
         elif msg.text == 'set name':
             user.state = 1
             bot.send_message(user.id,'enter your name: ',
-                             reply_markup=ReplyKeyboardMarkup()
+                             reply_markup=ReplyKeyboardRemove
                             )
                             
         elif msg.text == 'set age':    
             user.state = 2
             bot.send_message(user.id,'enter your age: ',
-                             reply_markup=ReplyKeyboardMarkup()
+                             reply_markup=ReplyKeyboardRemove
                             )
         elif msg.text == 'my profile':    
             
-            bot.send_message(user.id,f'Name: {user.name}\nAge: {user.age}',
-                             reply_markup=ReplyKeyboardMarkup()
-                            )
+            bot.send_message(user.id,f'Name: {user.name}\nAge: {user.age}')
 
         elif user.state == 1:
             user.name = msg.text
             user.state = 0
             bot.send_message(user.id,'your name saved: ',
-                    reply_markup=ReplyKeyboardMarkup()
+                    reply_markup=MAIN_KEYBOARD
                 )
         elif user.state == 2:
             user.age = msg.text
             user.state = 0
             bot.send_message(user.id,'your age saved: ',
-                    reply_markup=ReplyKeyboardMarkup()
+                    reply_markup=MAIN_KEYBOARD
                 )
 
     #     bot.send_message(chat_id, bot_text,
