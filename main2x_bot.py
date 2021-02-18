74310d21ff245494fd7e4958ea31f235027012a5
hrakbari77@gmail.com <hrakbari77@gmail.com>
2021-02-18 17:25:38 +0330

    user state checked

--- main2x_bot.py
+++ main2x_bot.py
@@ -28,38 +28,33 @@ def handle_message(bot: Client , msg: Message):
     user = check_user(msg.from_user.id)
     if (msg.chat.type != 'private'): #
         return
+
     if msg.text:
         if (msg.text == '/start' ):
-            bot.send_message(user.id, 'welcome' ,
-                                reply_markup=MAIN_KEYBOARD
-                            )
-        elif msg.text == 'set name':
+            bot.send_message(user.id, 'welcome' , reply_markup=MAIN_KEYBOARD )
+
+        elif(user.state == 0 and msg.text == 'set name' ) :
             user.state = 1
-            bot.send_message(user.id,'enter your name: ',
-                             reply_markup=ReplyKeyboardRemove
-                            )
+            bot.send_message(user.id,'enter your name: ', reply_markup=ReplyKeyboardRemove)
                             
-        elif msg.text == 'set age':    
+        elif (user.state == 1 and msg.text == 'set age') :    
             user.state = 2
-            bot.send_message(user.id,'enter your age: ',
-                             reply_markup=ReplyKeyboardRemove
-                            )
-        elif msg.text == 'my profile':    
-            
+            bot.send_message(user.id,'enter your age: ', reply_markup=ReplyKeyboardRemove)
+        
+        elif (user.state == 2 and msg.text == 'my profile'):    
             bot.send_message(user.id,f'Name: {user.name}\nAge: {user.age}')
 
         elif user.state == 1:
             user.name = msg.text
             user.state = 0
-            bot.send_message(user.id,'your name saved: ',
-                    reply_markup=MAIN_KEYBOARD
-                )
+            bot.send_message(user.id,'your name saved: ', reply_markup=MAIN_KEYBOARD)
+        
         elif user.state == 2:
             user.age = msg.text
             user.state = 0
-            bot.send_message(user.id,'your age saved: ',
-                    reply_markup=MAIN_KEYBOARD
-                )
+            bot.send_message(user.id,'your age saved: ', reply_markup=MAIN_KEYBOARD)
+
+
 
     #     bot.send_message(chat_id, bot_text,
     #                     reply_markup=ReplyKeyboardMarkup([['salam','khodafez'],['back']],resize_keyboard=True)
