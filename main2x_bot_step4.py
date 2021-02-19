f78d1acf13cf5f838dee0e7f324f592abbe882fe
hrakbari77@gmail.com <hrakbari77@gmail.com>
2021-02-20 01:13:30 +0330

    inline step4 fixed

--- main2x_bot_step4.py
+++ main2x_bot_step4.py
@@ -2,6 +2,11 @@ from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InlineQuery, \
     InlineQueryResultArticle, InputTextMessageContent
 
+'''
+    this code is an example of Inline action for telegram bot , 
+    if u wanna test this you have to type @yourbotusername and then space 
+    it will show a inline call then u press it and so on
+'''
 
 def IKM(data):
     return InlineKeyboardMarkup([[InlineKeyboardButton(text, cbd)] for text, cbd in data])
@@ -9,3 +14,20 @@ def IKM(data):
 
 client = Client('mybot')
 
+
+@client.on_callback_query()
+def handle_callback_query(bot: Client, query: CallbackQuery):
+    if query.data == 'start':
+        bot.edit_inline_text(query.inline_message_id, 'توضیحات بازی', reply_markup=IKM([('پایان', 'end')]))
+    elif query.data == 'end':
+        bot.edit_inline_text(query.inline_message_id, 'بازی تمام شد!!')
+
+
+@client.on_inline_query()
+def handle_inline_query(bot: Client, query: InlineQuery):
+    results = [InlineQueryResultArticle('شروع بازی جدید', InputTextMessageContent('متن بلند'),
+                                        reply_markup=IKM([('قبول بازی!', 'start')]))]
+    bot.answer_inline_query(query.id, results)
+
+
+client.run()
